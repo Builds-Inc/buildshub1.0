58cd3f863be96b80d24691521b523ac0bdf04169
我妻奏 <so03.isamtjkx@gmail.com>
2018-03-11 04:26:24 +0900

    余計なテーブル削除

--- buildshub/db/schema.rb
+++ buildshub/db/schema.rb
@@ -10,12 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180310182041) do
+ActiveRecord::Schema.define(version: 20180310184020) do
 
   create_table "students", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "sex"
+    t.string "college"
+    t.date "birth"
+    t.integer "graduate"
+    t.string "mail"
   end
 
 end
